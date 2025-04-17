569e47b2f4938d0fefbae99aa6282dd461f85ba8
quangthangk4 <quangthangkhmt.k22@gmail.com>
2025-04-17 16:48:01 +0700

    add cors

--- Readme.md
+++ Readme.md
@@ -3,3 +3,14 @@
 - vào đúng thu mục homeyolo
 - chạy lệnh: docker compose up -d (để chạy container mongodb)
 - có thể dùng mongo
+
+# 2. link adafruit
+- https://io.adafruit.com/quangthanga123/dashboards/name
+- tk: sanjzoro0@gmail.com
+- mk: lebinh000
+
+# 3. Chạy server
+- chạy trên intellij idea
+- hoặc chạy lệnh: .\mvnw spring-boot:run
+
+# 4. import postman
--- src/main/java/com/DADN/homeyolo/configuration/SecurityConfig.java
+++ src/main/java/com/DADN/homeyolo/configuration/SecurityConfig.java
@@ -4,9 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfig {
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            // Configure CORS settings here if needed
+            @Override
+            public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Replace with your frontend URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
+    }
 
 
     @Bean
